b3f4411b4992dd31b4e5aa4832f7be564f52c67d
Diego Casasola <diegoandrecf.dacf@gmail.com>
2023-06-29 02:52:35 -0400

    DC-feature nivel by usuario

--- soundFlow_models/api/nivel_viewset.py
+++ soundFlow_models/api/nivel_viewset.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from soundFlow_models.models import Nivel
+from soundFlow_models.models import Nivel, UserNivel
 
 
 class NivelSerializer(serializers.ModelSerializer):
@@ -12,3 +15,10 @@ class NivelSerializer(serializers.ModelSerializer):
 class NivelViewSet(viewsets.ModelViewSet):
     queryset = Nivel.objects.all()
     serializer_class = NivelSerializer
+
+    @action(detail=False, methods=['GET'], url_path='get_niveles_habilitados_user', url_name='Obtener niveles habilitados por usuario', permission_classes=[IsAuthenticated])
+    def get_niveles_habilitados_user(self, request):
+        nivel_user = [nivel['nivel_id'] for nivel in UserNivel.objects.filter(user_id=request.user.id).values('nivel_id')]
+        niveles = Nivel.objects.all()
+        niveles_habilitados = [niveles for niveles in niveles if niveles.id in nivel_user]
+        return Response(NivelSerializer(niveles_habilitados, many=True).data)
