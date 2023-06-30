a1036e0eb0f591f53d97d5b0216e4b5bc47c9d29
Diego Casasola <diegoandrecf.dacf@gmail.com>
2023-06-29 22:17:24 -0400

    DC-migraciones

--- soundFlow_models/models/__init__.py
+++ soundFlow_models/models/__init__.py
@@ -3,8 +3,9 @@ from .desafio import Desafio
 from .prueba import Prueba
 from .trofeo import Trofeo
 from .medalla import Medalla
+from .usuario_trofeos import UsuarioTrofeos
+from .usuario_medallas import UsuarioMedallas
 from .imagen import Imagen
 from .audio import Audio
 from .user_nivel import UserNivel
-from .usuario_trofeos import UsuarioTrofeos
-from .usuario_medallas import UsuarioMedallas
+from .pruebas_resueltas_user import PruebasResueltasUser
